5ad200b271533bb2e003c8fd7feb775099fc3352
RobMiller329 <66179978+RobMiller329@users.noreply.github.com>
2022-09-12 16:55:37 -0400

    Add files via upload

--- scripts/nameButton.js
+++ scripts/nameButton.js
@@ -16,49 +16,39 @@ trigger.addEventListener('click', async () =>
     let player7Name = document.getElementById("textPlayer7Name").value; let player8Name = document.getElementById("textPlayer8Name").value;
     let player9Name = document.getElementById("textPlayer9Name").value; let player10Name = document.getElementById("textPlayer10Name").value;
 
-    let seatA1 = document.getElementById("imageGroupPageSeatA1"); let seatA1c = document.getElementById("imageGroupPageSeatA1"); let seatNameA1 = document.getElementById("labelNameSeatA1WhoIs");
-    let seatA2 = document.getElementById("imageGroupPageSeatA2"); let seatA2c = document.getElementById("imageGroupPageSeatA2"); let seatNameA2 = document.getElementById("labelNameSeatA2WhoIs");
-    let seatA3 = document.getElementById("imageGroupPageSeatA3"); let seatA3c = document.getElementById("imageGroupPageSeatA3"); let seatNameA3 = document.getElementById("labelNameSeatA3WhoIs");
-    let seatA4 = document.getElementById("imageGroupPageSeatA4"); let seatA4c = document.getElementById("imageGroupPageSeatA4"); let seatNameA4 = document.getElementById("labelNameSeatA4WhoIs");
-    let seatA5 = document.getElementById("imageGroupPageSeatA5"); let seatA5c = document.getElementById("imageGroupPageSeatA5"); let seatNameA5 = document.getElementById("labelNameSeatA5WhoIs");
+    let seatA1 = document.getElementById("imageGroupPageSeatA1"); let seatA1c = document.getElementById("imageGroupPageSeatA1c"); let seatNameA1 = document.getElementById("labelNameSeatA1WhoIs");
+    let seatA2 = document.getElementById("imageGroupPageSeatA2"); let seatA2c = document.getElementById("imageGroupPageSeatA2c"); let seatNameA2 = document.getElementById("labelNameSeatA2WhoIs");
+    let seatA3 = document.getElementById("imageGroupPageSeatA3"); let seatA3c = document.getElementById("imageGroupPageSeatA3c"); let seatNameA3 = document.getElementById("labelNameSeatA3WhoIs");
+    let seatA4 = document.getElementById("imageGroupPageSeatA4"); let seatA4c = document.getElementById("imageGroupPageSeatA4c"); let seatNameA4 = document.getElementById("labelNameSeatA4WhoIs");
+    let seatA5 = document.getElementById("imageGroupPageSeatA5"); let seatA5c = document.getElementById("imageGroupPageSeatA5c"); let seatNameA5 = document.getElementById("labelNameSeatA5WhoIs");
 
-    let seatB1 = document.getElementById("imageGroupPageSeatB1"); let seatB1c = document.getElementById("imageGroupPageSeatB1"); let seatNameB1 = document.getElementById("labelNameSeatB1WhoIs");
-    let seatB2 = document.getElementById("imageGroupPageSeatB2"); let seatB2c = document.getElementById("imageGroupPageSeatB2"); let seatNameB2 = document.getElementById("labelNameSeatB2WhoIs");
-    let seatB3 = document.getElementById("imageGroupPageSeatB3"); let seatB3c = document.getElementById("imageGroupPageSeatB3"); let seatNameB3 = document.getElementById("labelNameSeatB3WhoIs");
-    let seatB4 = document.getElementById("imageGroupPageSeatB4"); let seatB4c = document.getElementById("imageGroupPageSeatB4"); let seatNameB4 = document.getElementById("labelNameSeatB4WhoIs");
-    let seatB5 = document.getElementById("imageGroupPageSeatB5"); let seatB5c = document.getElementById("imageGroupPageSeatB5"); let seatNameB5 = document.getElementById("labelNameSeatB5WhoIs");
+    let seatB1 = document.getElementById("imageGroupPageSeatB1"); let seatB1c = document.getElementById("imageGroupPageSeatB1c"); let seatNameB1 = document.getElementById("labelNameSeatB1WhoIs");
+    let seatB2 = document.getElementById("imageGroupPageSeatB2"); let seatB2c = document.getElementById("imageGroupPageSeatB2c"); let seatNameB2 = document.getElementById("labelNameSeatB2WhoIs");
+    let seatB3 = document.getElementById("imageGroupPageSeatB3"); let seatB3c = document.getElementById("imageGroupPageSeatB3c"); let seatNameB3 = document.getElementById("labelNameSeatB3WhoIs");
+    let seatB4 = document.getElementById("imageGroupPageSeatB4"); let seatB4c = document.getElementById("imageGroupPageSeatB4c"); let seatNameB4 = document.getElementById("labelNameSeatB4WhoIs");
+    let seatB5 = document.getElementById("imageGroupPageSeatB5"); let seatB5c = document.getElementById("imageGroupPageSeatB5c"); let seatNameB5 = document.getElementById("labelNameSeatB5WhoIs");
 
-    if(seatA1c.style.display == "block"){ seatA1c.style.display = "none"; };
-    if(seatA2c.style.display == "block"){ seatA2c.style.display = "none"; };
-    if(seatA3c.style.display == "block"){ seatA3c.style.display = "none"; };
-    if(seatA4c.style.display == "block"){ seatA4c.style.display = "none"; };
-    if(seatA5c.style.display == "block"){ seatA5c.style.display = "none"; };
-
-    if(seatB1c.style.display == "block"){ seatB1c.style.display = "none"; };
-    if(seatB2c.style.display == "block"){ seatB2c.style.display = "none"; };
-    if(seatB3c.style.display == "block"){ seatB3c.style.display = "none"; };
-    if(seatB4c.style.display == "block"){ seatB4c.style.display = "none"; };
-    if(seatB5c.style.display == "block"){ seatB5c.style.display = "none"; };
-
-    if(seatNameA1.style.display == "block"){ seatNameA1.style.display = "none"; };
-    if(seatNameA2.style.display == "block"){ seatNameA2.style.display = "none"; };
-    if(seatNameA3.style.display == "block"){ seatNameA3.style.display = "none"; };
-    if(seatNameA4.style.display == "block"){ seatNameA4.style.display = "none"; };
-    if(seatNameA5.style.display == "block"){ seatNameA5.style.display = "none"; };
-
-    if(seatNameB1.style.display == "block"){ seatNameB1.style.display = "none"; };
-    if(seatNameB2.style.display == "block"){ seatNameB2.style.display = "none"; };
-    if(seatNameB3.style.display == "block"){ seatNameB3.style.display = "none"; };
-    if(seatNameB4.style.display == "block"){ seatNameB4.style.display = "none"; };
-    if(seatNameB5.style.display == "block"){ seatNameB5.style.display = "none"; };
-    
     let arrayOfNames = [player1Name, player2Name, player3Name, player4Name, player5Name, player6Name, player7Name, player8Name, player9Name, player10Name];
     let arrayOfAllPrimarySeats = [seatA1, seatA2, seatA3, seatA4, seatA5, seatB1, seatB2, seatB3, seatB4, seatB5];
     let arrayOfAllComplementarySeats = [seatA1c, seatA2c, seatA3c, seatA4c, seatA5c, seatB1c, seatB2c, seatB3c, seatB4c, seatB5c];
     let arrayOfSeatNameLabels = [seatNameA1, seatNameA2, seatNameA3, seatNameA4, seatNameA5, seatNameB1, seatNameB2, seatNameB3, seatNameB4, seatNameB5];
 
+    //replaces series of if statements; shorter and scales better
+    for(let i = 0; i < arrayOfAllPrimarySeats; i++)
+    {
+        if(arrayOfAllComplementarySeats[i].style.display == "block")
+        {
+            arrayOfAllComplementarySeats[i].style.display = "none";
+        }
+
+        if(arrayOfSeatNameLabels[i].style.display == "block")
+        {
+            arrayOfSeatNameLabels[i].style.display = "none";
+        }
+    }
+
     arrayOfNames = playerShuffler(arrayOfNames);
-    loopThroughSeats(playerCount, arrayOfNames, arrayOfAllPrimarySeats, arrayOfAllComplementarySeats, arrayOfSeatNameLabels);
+    loopThroughSeats(playerCount, arrayOfNames, arrayOfAllPrimarySeats, arrayOfSeatNameLabels);
 });
 
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -80,9 +70,9 @@ function playerShuffler(nameArray)
     return nameArray;
 }
 
-function loopThroughSeats(playerCount, arrayOfNames, arrayOfAllPrimarySeats, arrayOfAllComplementarySeats, arrayOfSeatNameLabels)
+function loopThroughSeats(playerCount, arrayOfNames, arrayOfAllPrimarySeats, arrayOfSeatNameLabels)
 {
-    let arrayOfIncomingPics = [];
+    let imagesToChange = [];
 
     for(let i = 0; i < arrayOfAllPrimarySeats.length; i++)
     {
@@ -91,15 +81,13 @@ function loopThroughSeats(playerCount, arrayOfNames, arrayOfAllPrimarySeats, arr
             arrayOfAllPrimarySeats[i].style.display = "block";
             arrayOfSeatNameLabels[i].style.display = "block";
             arrayOfSeatNameLabels[i].innerHTML = arrayOfNames[i];
-            arrayOfIncomingPics.push(arrayOfAllPrimarySeats[i].id);
+            imagesToChange.push(arrayOfAllPrimarySeats[i].id);
         }else
         {
-            let currentSeat = document.getElementById(arrayOfAllPrimarySeats[i].id);
-            let currentLabel = document.getElementById(arrayOfSeatNameLabels[i].id);
-            currentSeat.style.display = "none";
-            currentLabel.style.display = "none";
+            arrayOfAllPrimarySeats[i].style.display = "none";
+            arrayOfSeatNameLabels[i].style.display = "none";
         }
     }
 
-    groupAPIBuild(arrayOfIncomingPics);
+    groupAPIBuild(imagesToChange);
 }
--- scripts/nameInputs.js
+++ scripts/nameInputs.js
@@ -11,103 +11,28 @@ const textPlayer8Name = document.getElementById("textPlayer8Name"); const labelP
 const textPlayer9Name = document.getElementById("textPlayer9Name"); const labelPlayer9Name = document.getElementById("labelPlayer9Name");
 const textPlayer10Name = document.getElementById("textPlayer10Name"); const labelPlayer10Name = document.getElementById("labelPlayer10Name");
 
-const playerCount = document.getElementById("dropdownSelectPlayerCount");
-playerCount.onchange = function()
+const arrayOfTextPlayerNames = [textPlayer1Name, textPlayer2Name, textPlayer3Name, textPlayer4Name, textPlayer5Name, textPlayer6Name, textPlayer7Name, textPlayer8Name, textPlayer9Name, textPlayer10Name];
+const arrayOfLabelPlayerNames = [labelPlayer1Name, labelPlayer2Name, labelPlayer3Name, labelPlayer4Name, labelPlayer5Name, labelPlayer6Name, labelPlayer7Name, labelPlayer8Name, labelPlayer9Name, labelPlayer10Name];
+
+const playerCountDropdown = document.getElementById("dropdownSelectPlayerCount");
+playerCountDropdown.onchange = function()
 {
-    switch(playerCount.value)
+    console.log(labelPlayer1Name);
+    console.log(arrayOfLabelPlayerNames[0]);
+    console.log(arrayOfLabelPlayerNames);
+    let playerCountNumber = playerCountDropdown.value
+    for(let i = 0; i < arrayOfTextPlayerNames.length; i++)
     {
-        case '2':
-            textPlayer3Name.style.display = "none"; labelPlayer3Name.style.display = "none";
-            textPlayer4Name.style.display = "none"; labelPlayer4Name.style.display = "none";
-            textPlayer5Name.style.display = "none"; labelPlayer5Name.style.display = "none";
-            textPlayer6Name.style.display = "none"; labelPlayer6Name.style.display = "none";
-            textPlayer7Name.style.display = "none"; labelPlayer7Name.style.display = "none";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '3':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "none"; labelPlayer4Name.style.display = "none";
-            textPlayer5Name.style.display = "none"; labelPlayer5Name.style.display = "none";
-            textPlayer6Name.style.display = "none"; labelPlayer6Name.style.display = "none";
-            textPlayer7Name.style.display = "none"; labelPlayer7Name.style.display = "none";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '4':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "none"; labelPlayer5Name.style.display = "none";
-            textPlayer6Name.style.display = "none"; labelPlayer6Name.style.display = "none";
-            textPlayer7Name.style.display = "none"; labelPlayer7Name.style.display = "none";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '5':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "none"; labelPlayer6Name.style.display = "none";
-            textPlayer7Name.style.display = "none"; labelPlayer7Name.style.display = "none";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '6':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "block"; labelPlayer6Name.style.display = "block";
-            textPlayer7Name.style.display = "none"; labelPlayer7Name.style.display = "none";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '7':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "block"; labelPlayer6Name.style.display = "block";
-            textPlayer7Name.style.display = "block"; labelPlayer7Name.style.display = "block";
-            textPlayer8Name.style.display = "none"; labelPlayer8Name.style.display = "none";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '8':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "block"; labelPlayer6Name.style.display = "block";
-            textPlayer7Name.style.display = "block"; labelPlayer7Name.style.display = "block";
-            textPlayer8Name.style.display = "block"; labelPlayer8Name.style.display = "block";
-            textPlayer9Name.style.display = "none"; labelPlayer9Name.style.display = "none";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '9':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "block"; labelPlayer6Name.style.display = "block";
-            textPlayer7Name.style.display = "block"; labelPlayer7Name.style.display = "block";
-            textPlayer8Name.style.display = "block"; labelPlayer8Name.style.display = "block";
-            textPlayer9Name.style.display = "block"; labelPlayer9Name.style.display = "block";
-            textPlayer10Name.style.display = "none"; labelPlayer10Name.style.display = "none";
-            break;
-        case '10':
-            textPlayer3Name.style.display = "block"; labelPlayer3Name.style.display = "block";
-            textPlayer4Name.style.display = "block"; labelPlayer4Name.style.display = "block";
-            textPlayer5Name.style.display = "block"; labelPlayer5Name.style.display = "block";
-            textPlayer6Name.style.display = "block"; labelPlayer6Name.style.display = "block";
-            textPlayer7Name.style.display = "block"; labelPlayer7Name.style.display = "block";
-            textPlayer8Name.style.display = "block"; labelPlayer8Name.style.display = "block";
-            textPlayer9Name.style.display = "block"; labelPlayer9Name.style.display = "block";
-            textPlayer10Name.style.display = "block"; labelPlayer10Name.style.display = "block";
-            break;
-        default:
-            console.log("playerCount.value switch has defaulted");
-            break;
+        if(i < playerCountNumber)
+        {
+            arrayOfTextPlayerNames[i].style.display = "block";
+            arrayOfLabelPlayerNames[i].style.display = "block";
+        }else
+        {
+            console.log("loop " + i + " is NOT less than " + playerCountNumber);
+            console.log("text: " + arrayOfTextPlayerNames[i] + "  label: " + arrayOfLabelPlayerNames[i]);
+            arrayOfTextPlayerNames[i].style.display = "none";
+            arrayOfLabelPlayerNames[i].style.display = "none";
+        }
     }
 };
